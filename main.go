88f45b08104cf6b77c43fe9b280adcd230d0bfc8
shn27 <sohanur1525sohan@gmail.com>
2024-05-09 13:42:45 +0600

    Initialize kubeconfig.Tested

--- main.go
+++ main.go
@@ -5,33 +5,52 @@ import (
 	"fmt"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+	_ "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"log"
+	"os"
+	"path/filepath"
 	"time"
 )
 
 func main() {
-	kubeconfig := flag.String("kubeconfig", "/home/sohan/.kube/config", "location to your kubeconfig file")
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		// handle error
-		fmt.Printf("erorr %s building config from flags\n", err.Error())
-		config, err = rest.InClusterConfig()
-		if err != nil {
-			fmt.Printf("error %s, getting inclusterconfig", err.Error())
-		}
-	}
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		// handle error
-		fmt.Printf("error %s, creating clientset\n", err.Error())
-	}
-	fmt.Println(clientset)
-
+	kubeConfigFilePath := flag.String("kubeconfig", filepath.Join(homedir.HomeDir(), ".kube", "config"), "kubeconfig file path")
+	flag.Parse()
+	clientset := InitializeClientGo(*kubeConfigFilePath)
 	ch := make(chan struct{})
 	informers := informers.NewSharedInformerFactory(clientset, 10*time.Minute)
 	c := newController(clientset, informers.Apps().V1().Deployments())
 	informers.Start(ch)
 	c.run(ch)
 	fmt.Println(informers)
+}
+
+func InitializeClientGo(kubeConfigFilePath string) *kubernetes.Clientset {
+	if pathExists := func(path string) bool {
+		if _, err := os.Stat(path); err != nil {
+			log.Println("Path " + path + " does not exist. Loading incluster config")
+			return false
+		}
+		return true
+	}(kubeConfigFilePath); !pathExists {
+		kubeConfigFilePath = ""
+		log.Println("Kubeconfig path doesn't exist. using the inClusterConfig")
+	} else {
+		log.Println("Loading kubeconfig file " + kubeConfigFilePath)
+	}
+
+	var err error
+	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigFilePath)
+	if err != nil {
+		log.Fatal("error loading kubeconfig ", err)
+	}
+
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal("error creating clientset", err)
+	}
+
+	log.Println("Successfully loaded kubeconfig")
+	return clientSet
 }
